01dc84652ececd54484378e055a3ae3569379cdb
protaxY <feorov2001@mail.ru>
2020-12-05 22:47:47 +0300

    it works!!

--- child.c
+++ child.c
@@ -13,31 +13,31 @@ int main(int argc, char* argv[]){
     }
 
     int mutex1 = shm_open(argv[2], O_RDWR | O_CREAT, S_IRWXU);
-    int cond1 = shm_open(argv[3], O_RDWR | O_CREAT, S_IRWXU);
+    int mutex2 = shm_open(argv[3], O_RDWR | O_CREAT, S_IRWXU);
     int pipe1 = shm_open(argv[4], O_RDWR | O_CREAT, S_IRWXU);
     int pipe1Size = shm_open(argv[5], O_RDWR | O_CREAT, S_IRWXU);
 
     pthread_mutex_t* mutex = (pthread_mutex_t *) mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, mutex1, 0);
-    pthread_cond_t* cond = (pthread_cond_t *) mmap(NULL, sizeof(pthread_cond_t), PROT_READ | PROT_WRITE, MAP_SHARED, cond1, 0);
+    pthread_mutex_t* secondMutex = (pthread_mutex_t *) mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, mutex2, 0);
     float* mmfData = (float *) mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_SHARED, pipe1, 0);
     int* mmfDataSize = (int *) mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_SHARED, pipe1Size, 0);
 
-
-
-    //printf("loh");
-    while (*mmfDataSize != -1){
+    while (*mmfDataSize != -2){
+        printf("hi\n");
         pthread_mutex_lock(mutex);
-        printf("ahahahah %d\n", *mmfDataSize);
+        while (*mmfDataSize == -1){
+            pthread_cond_wait(cond, mutex);
+        }
+        if (*mmfDataSize == -2){
+            break;
+        }
         float result = 0;
         for (int i = 0; i < *mmfDataSize; ++i){
             result += mmfData[i];
         }
         fprintf(file, "%f\n", result);
-        printf("%f\n", result);
-        *mmfDataSize = -2;
-        pthread_cond_signal(cond);
-        printf("loh");
-        pthread_mutex_unlock(mutex);
+        *mmfDataSize = -1;
+        pthread_mutex_unlock(secondMutex);
     }
     printf("ne loh");
     fclose(file);
--- main.c
+++ main.c
@@ -5,72 +5,100 @@
 #include <fcntl.h>
 #include <pthread.h>
 
-int main(int argc, char* argv[]){
+int main(){
     printf("enter file name:");
     char filename[256];
     scanf("%s", filename);
 
     char pipeName[] = "pipe";
     char mutexName[] = "mutex";
+    char mutex2Name[] = "mutex2";
     char pipe1SizeName[] = "size";
-    char condName[] = "cond";
     int pipe1 = shm_open(pipeName, O_RDWR | O_CREAT, S_IRWXU);
     int pipe1Size = shm_open(pipe1SizeName, O_RDWR | O_CREAT, S_IRWXU);
     int mutex1 = shm_open(mutexName, O_RDWR | O_CREAT, S_IRWXU);
-    int cond1 = shm_open(condName, O_RDWR | O_CREAT, S_IRWXU);
+    int mutex2 = shm_open(mutex2Name, O_RDWR | O_CREAT, S_IRWXU);
+    if (pipe1 == -1 || pipe1Size == -1 || mutex1 == -1 || mutex2 == -1){
+        perror("shm_open\n");
+        return -1;
+    }
 
-    ftruncate(pipe1, getpagesize());
-    ftruncate(pipe1Size, sizeof(int));
-    ftruncate(mutex1, sizeof(pthread_mutex_t));
-    ftruncate(cond1, sizeof(pthread_cond_t));
+    if (ftruncate(pipe1, getpagesize()) == -1){
+        perror("ftruncate error\n");
+        return -1;
+    }
+    if (ftruncate(pipe1Size, sizeof(int)) == -1){
+        perror("ftruncate error\n");
+        return -1;
+    }
+    if (ftruncate(mutex1, sizeof(pthread_mutex_t))){
+        perror("ftruncate error\n");
+        return -1;
+    }
+    if (ftruncate(mutex2, sizeof(pthread_mutex_t))){
+        perror("ftruncate error\n");
+        return -1;
+    }
+
     float* mmfData = (float *) mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_SHARED, pipe1, 0);
     int* mmfDataSize = (int *) mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_SHARED, pipe1Size, 0);
-    *mmfDataSize = 0;
+    *mmfDataSize = -1;
     pthread_mutex_t* mutex = (pthread_mutex_t *) mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, mutex1, 0);
+    pthread_mutex_t* secondMutex = (pthread_mutex_t *) mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, mutex2, 0);
     pthread_mutexattr_t mutexAttribute;
     pthread_mutexattr_init(&mutexAttribute);
     pthread_mutexattr_setpshared(&mutexAttribute, PTHREAD_PROCESS_SHARED);
     pthread_mutex_init(mutex, &mutexAttribute);
+    pthread_mutex_init(secondMutex, &mutexAttribute);
 
-    pthread_cond_t* cond = (pthread_cond_t *) mmap(NULL, sizeof(pthread_cond_t), PROT_READ | PROT_WRITE, MAP_SHARED, cond1, 0);
-    pthread_condattr_t condAttribute;
-    pthread_condattr_init(&condAttribute);
-    pthread_condattr_setpshared(&condAttribute, PTHREAD_PROCESS_SHARED);
-    pthread_cond_init(cond, &condAttribute);
-
-
-    int id = fork();
-    if (id == -1){
-        printf("fork error\n");
+    if (pthread_mutex_lock(secondMutex)){
+        perror("pthread_mutex_lock error\n");
         return -1;
     }
-    else if (id == 0){
-        char* argv[] = {"child", filename, mutexName, condName, pipeName, pipe1SizeName, (char *)NULL};
+    if (pthread_mutex_lock(mutex)){
+        perror("pthread_mutex_lock error\n");
+        return -1;
+    }
+    int id = fork();
+    if (id == -1){
+        perror("fork error\n");
+        return -1;
+    } else if (id == 0){
+        char* argv[] = {"child", filename, mutexName, mutex2Name, pipeName, pipe1SizeName, (char *)NULL};
         if (execv("child", argv) == -1){
             perror("execl error\n");
+            return -1;
         }
-    }
-    else{
+    } else {
         printf("insert sum terms:");
         float currentTerm;
         char c;
-        pthread_mutex_lock(mutex);
+        printf("retard1\n");
         int cnt = 0;
-        printf("retard\n");
+        printf("retard2\n");
         while(scanf("%f%c", &currentTerm, &c) > 0){
             mmfData[cnt] = currentTerm;
             ++cnt;
             if (c == '\n'){
                 *mmfDataSize = cnt;
-                while (*mmfDataSize != -2){
-                    printf("ok\n");
-                    pthread_cond_wait(cond, mutex);
+                if (pthread_mutex_unlock(mutex) != 0){
+                    perror("pthread_mutex_unlock error\n");
+                    return -1;
                 }
+                if (pthread_mutex_lock(secondMutex)){
+                    perror("pthread_mutex_lock error\n");
+                    return -1;
+                }
+                printf("retard3\n");
                 cnt = 0;
             }
         }
-        *mmfDataSize = -1;
-        pthread_mutex_unlock(mutex);
+        printf("retard4\n");
+        *mmfDataSize = -2;
+        if (pthread_mutex_unlock(mutex)){
+            perror("pthread_mutex_unlock error\n");
+            return -1;
+        }
     }
     return 0;
 }
